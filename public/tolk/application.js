e32021e1e15cef8f7ed896feae0bf604e902df75
Simon Tokumine <simon@japancentre.com>
2010-07-18 11:26:03 +0100

    don't overwrite existing translations with google translate on click

--- public/tolk/application.js
+++ public/tolk/application.js
@@ -8,8 +8,8 @@ Event.observe(window, 'load', function() {
  		var phrase = this.up('tr').down('span.to_translate');
 		var textarea = this;
 		var target_language = $('target_language').innerHTML;
-
-		if (phrase && textarea && $F(textarea).blank){
+		
+		if (phrase && textarea && $F(textarea).blank()){
 			//translate it using google translate, autodetect source language			
 			var to_trans = phrase.innerHTML;			
 			google.language.translate(to_trans, "", target_language, function(result) {
